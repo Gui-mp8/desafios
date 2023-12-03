d4ff37a40307b16e0655a018f2f6276c8e71397f
Guilherme Machado <guilherme.machado@indicium.tech>
2023-12-03 13:15:04 -0300

    .

---
+++ array_string/remove_element.py
@@ -0,0 +1,11 @@
+from typing import List
+
+def remove_element(nums: List[int], val: int) -> int:
+    index = 0
+    for i in range(len(nums)):
+        if nums[i] != val:
+            nums[index] = nums[i]
+            index += 1
+    return index
+
+print(remove_element(nums = [0,1,2,2,3,0,4,2], val = 2))
